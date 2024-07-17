e496480a43edaf099db4c4cc43ca0be4539b3d6d
TobyL98 <tobylawrence@btinternet.com>
2024-07-17 12:12:58 +0100

    calculating differences in the secretion fluxes

---
+++ first_model/Results/secretion.py
@@ -0,0 +1,14 @@
+################
+# secretion.py
+################
+
+
+'''Calculate the difference in secretion flux
+between two model secretion outputs from PyCoMo'''
+
+import pandas as pd
+from pathlib import Path
+
+def secretion_diff():
+    '''Calculates the difference in secretion flux
+    between two csv secretion outputs'''
